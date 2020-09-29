a3363a1373e3255895ea45b259da181cfae0173a
AntonioStracquadanio <72078324+AntonioStracquadanio@users.noreply.github.com>
2020-09-29 13:00:05 +0100

    Update Datalist.m

--- Datalist.m
+++ Datalist.m
@@ -3,8 +3,7 @@ cnt = 0;
 cnt = cnt+1;
 dat(cnt).d_path = 'INSERT_PATH_OF_FOLDER_HERE';
 dat(cnt).ser_flow = [INSERT_MAGNITUDE_PHASE_SERIES_NUMBERS_HERE_SEPARATED_BY_COMMAS];
-dat(cnt).contour_file = 'contours.cvi42wsx';
-
+dat(cnt).contour_file = 'contours.cvi
  
 
 
